4c54e83e54673e3cd4e6feb1a06840d7a16aad9c
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-21 17:18:21 +0200

    Update query1.sql

--- Q01__find_duplicates/query1.sql
+++ Q01__find_duplicates/query1.sql
@@ -1,1 +1,6 @@
-# 1. Find duplicates by Email Address
+-- 1.Find duplicates email addresses
+SELECT EmailAddress, COUNT(*) AS Num_EmailAddress
+FROM [AdventureWorksDW2020].[dbo].[DimCustomer]
+GROUP BY  EmailAddress
+HAVING COUNT(*) >1
+ORDER BY Num_EmailAddress DESC;
