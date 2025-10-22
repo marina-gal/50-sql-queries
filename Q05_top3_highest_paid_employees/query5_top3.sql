faf8d02db9d999634c0094fd523c48184a1ab5bd
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-22 19:31:02 +0200

    Update query5_top3.sql

--- Q05_top3_highest_paid_employees/query5_top3.sql
+++ Q05_top3_highest_paid_employees/query5_top3.sql
@@ -1,3 +1,9 @@
-select top 3 *
-from AdventureWorks2019.HumanResources.EmployeePayHistory
+select top 3
+		eph.BusinessEntityID,
+		eph.Rate,
+		pp.FirstName,
+		pp.LastName
+		
+from AdventureWorks2019.HumanResources.EmployeePayHistory as eph
+left join AdventureWorks2019.Person.Person as pp on pp.BusinessEntityID = eph.BusinessEntityID
 order by Rate desc
