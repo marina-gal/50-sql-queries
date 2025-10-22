3a067e1acbcf37b30a385ba6b88b4b8abd80bc1d
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-22 18:13:37 +0200

    Update query3.sql

--- Q03_find_employees_without_dep/query3.sql
+++ Q03_find_employees_without_dep/query3.sql
@@ -1,1 +1,14 @@
+select e.BusinessEntityID as EmployeeID
+      ,e.JobTitle 
+      ,pp.FirstName 
+      ,pp.LastName 
+	  ,edh.DepartmentID
 
+
+from AdventureWorks2019.HumanResources.Employee as e
+		LEFT JOIN AdventureWorks2019.HumanResources.EmployeeDepartmentHistory as edh
+				on e.BusinessEntityID = edh.BusinessEntityID
+		LEFT JOIN AdventureWorks2019.Person.Person as pp
+				on pp.BusinessEntityID = e.BusinessEntityID
+
+where edh.DepartmentID IS NULL
