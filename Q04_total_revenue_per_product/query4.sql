2f4b2e11b10e95e2a1194eb7d951a0e106bb2364
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-22 19:14:44 +0200

    Update query4.sql

--- Q04_total_revenue_per_product/query4.sql
+++ Q04_total_revenue_per_product/query4.sql
@@ -1,1 +1,10 @@
-
+-- Find the total revenue per product
+select pp.ProductID,
+       pp.Name as ProductName, 
+	   SUM(sod.OrderQty*sod.UnitPrice*(1-sod.UnitPriceDiscount)) as Total_Revenue
+          
+from [AdventureWorks2019].[Sales].[SalesOrderDetail] as sod
+	LEFT JOIN AdventureWorks2019.Production.Product as pp
+			on pp.ProductID = sod.ProductID
+group by pp.ProductID, pp.Name 
+order by Total_Revenue desc
