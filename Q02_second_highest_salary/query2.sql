aa2dd5ed01439a5ff24da3f374e89c364d8ec5fe
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-22 14:06:55 +0200

    Update query2.sql

--- Q02_second_highest_salary/query2.sql
+++ Q02_second_highest_salary/query2.sql
@@ -1,1 +1,12 @@
+-- Retrieve the second highest Rate from the Employee table
+select MAX(Rate) as SecondHighestRate
+from [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+where Rate < (select MAX(Rate*40*52)
+			  from [AdventureWorks2019].[HumanResources].[EmployeePayHistory]);
 
+-- To calculate the second highest annual salary based on aproximately 40 hours of work per week and 52 weeks.
+select MAX(Rate*40*52) as SecondHighestAnnualSalary
+from [AdventureWorks2019].[HumanResources].[EmployeePayHistory]
+where Rate*40*52 < (
+					select MAX(Rate*40*52)
+					from [AdventureWorks2019].[HumanResources].[EmployeePayHistory])
