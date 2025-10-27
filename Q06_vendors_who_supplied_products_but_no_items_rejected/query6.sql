fb62d3cdbe6b2cca99bf0d8f42bf0e499e740ae7
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-27 12:13:24 +0100

    Update query6.sql

--- Q06_vendors_who_supplied_products_but_no_items_rejected/query6.sql
+++ Q06_vendors_who_supplied_products_but_no_items_rejected/query6.sql
@@ -1,1 +1,14 @@
+-- Returning all vendors who supplied products BUT none of whose orders contained rejected products.
 
+select distinct
+		 v.BusinessEntityID as VendorID
+		,v.Name as VendorName
+		,pod.RejectedQty
+
+from AdventureWorks2019.Purchasing.Vendor as v
+	left join AdventureWorks2019.Purchasing.PurchaseOrderHeader as poh
+		on poh.VendorID = v.BusinessEntityID
+	left join AdventureWorks2019.Purchasing.PurchaseOrderDetail as pod
+		on pod.PurchaseOrderID = poh.PurchaseOrderID
+	AND pod.RejectedQty > 0
+where pod.PurchaseOrderDetailID IS NULL
