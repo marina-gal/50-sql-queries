34858203958c66b8ce5b5208882ac3b725ac4136
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-27 15:56:44 +0100

    Update query7.sql

--- Q07_nb_orders_per_customer/query7.sql
+++ Q07_nb_orders_per_customer/query7.sql
@@ -1,1 +1,15 @@
+-- Number of orders per customer
 
+select 
+ soh.CustomerID
+,CONCAT (p.FirstName, ' ', p.LastName) as FullName
+,COUNT(soh.SalesOrderID) as nb_orders_per_customer
+
+from [AdventureWorks2019].[Sales].[SalesOrderHeader] as soh
+	left join [AdventureWorks2019].[Sales].[Customer] as c
+		on c.CustomerID = soh.CustomerID
+	left join [AdventureWorks2019].[Person].[Person] as p
+		on p.BusinessEntityID = c.PersonID
+	
+group by soh.CustomerID, p.FirstName, p.LastName
+order by nb_orders_per_customer DESC
