18f97c1caed5cb09907eba0d1581b4f3c3d75166
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-27 18:39:06 +0100

    Update query11.sql

--- Q11_products_where_never_sold/query11.sql
+++ Q11_products_where_never_sold/query11.sql
@@ -1,1 +1,12 @@
+-- Products that where never sold
+SELECT p.[ProductID]
+      ,p.[Name]
+      ,p.[ProductNumber]
+      ,p.[ListPrice]
+      
+FROM [AdventureWorks2019].[Production].[Product] AS p
+LEFT JOIN AdventureWorks2019.Sales.SalesOrderDetail AS sod
+ON p.ProductID = sod.ProductID
 
+WHERE sod.ProductID IS NULL 
+ORDER BY p.ProductID;
