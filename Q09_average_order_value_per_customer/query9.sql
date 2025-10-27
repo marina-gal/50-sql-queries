94f301cf7026433836708c993f103b813effe119
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-10-27 17:17:22 +0100

    Update query9.sql

--- Q09_average_order_value_per_customer/query9.sql
+++ Q09_average_order_value_per_customer/query9.sql
@@ -1,1 +1,14 @@
+-- How much is the average order value per customer ?
+SELECT      
+       soh.[CustomerID]   
+	  ,AVG(soh.TotalDue) AS Avg_order_value
+	  ,CONCAT(pp.FirstName, ' ',pp.LastName) AS FullName
+ 
+FROM [AdventureWorks2019].[Sales].[SalesOrderHeader] AS soh
+	LEFT JOIN AdventureWorks2019.Sales.Customer AS sc
+		ON  sc.CustomerID = soh.CustomerID
+	LEFT JOIN AdventureWorks2019.Person.Person AS pp
+		ON pp.BusinessEntityID = sc.PersonID
 
+GROUP BY soh.[CustomerID], pp.FirstName, pp.LastName
+ORDER BY Avg_order_value DESC;
